3dd260f8ff823a5415c8631d3e1e4de4b14bb238
Donquixot228 <64096633+Donquixot228@users.noreply.github.com>
2022-10-25 16:09:01 +0300

    hide personal api key

--- lib/api_client.dart
+++ lib/api_client.dart
@@ -7,7 +7,7 @@ class ApiClient {
         "POST", Uri.parse("https://api.remove.bg/v1.0/removebg"));
     request.files
         .add(await http.MultipartFile.fromPath("image_file", imagePath));
-    request.headers.addAll({"X-API-Key": "ndHvBFj7SN3oyYiFG6EViNYs"}); //Put Your API key HERE
+    request.headers.addAll({"X-API-Key": "1234567895ff"}); //Put Your API key HERE. You can find it in the profile page in https://www.remove.bg/ru/dashboard
     final response = await request.send();
     if (response.statusCode == 200) {
       http.Response imgRes = await http.Response.fromStream(response);
